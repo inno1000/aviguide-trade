2d260e3d93e329b39341b8ee1684cec4aa0ba6c8
The Pocket 006 <temaniigor@gmail.com>
2019-11-15 17:07:34 +0100

    Good

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -18,23 +18,59 @@ Class MY_Controller extends CI_Controller{
         parent::__construct();
     }
 
-    protected function render($view = 'index', $titre = '', $header = true)
+    protected function render($view = 'index', $title = '', $header = true)
     {
         if(!$view) $view = 'index';
-        if(!$titre) $titre = 'Accueil';
+        if(!$title) $title = 'Accueil';
 
-        $this->load->view('head', ['title'=>$titre]);
+        $this->load->view('head', ['title'=>$title]);
         if($header === true) $this->load->view('header');
         $this->execute($view);
         $this->load->view('footer');
     }
 
-    protected function execute($view, $titre='', $return = FALSE)
+    protected function execute($view, $title='', $return = FALSE)
     {
         $this->zone = trim($this->zone);
 
-        if($titre) $this->data['titre'] = $titre;
+        if($title) $this->data['titre'] = $title;
         if($this->zone) $view = $this->zone.'/'.$view;
+
+        return $this->load->view($view, $this->data, $return);
+    }
+}
+
+Class MY_AdminController extends MY_Controller{
+
+    /**
+     * @var string
+     */
+    protected $adminViewZone = 'admin';
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    protected function render($view = 'index', $title = '', $header = true)
+    {
+        $this->adminViewZone = trim($this->adminViewZone);
+
+        if(!$view) $view = 'index';
+        if(!$title) $title = 'Accueil';
+
+        $this->load->view($this->adminViewZone.'/head', ['title'=>$title]);
+        $this->load->view($this->adminViewZone.'/header');
+        $this->execute($view);
+        $this->load->view($this->adminViewZone.'/footer');
+    }
+
+    protected function execute($view, $title='', $return = FALSE)
+    {
+        $this->zone = trim($this->zone);
+
+        if($title) $this->data['title'] = $title;
+        $view = $this->adminViewZone.(($this->zone)?'/'.$this->zone:'').'/'.$view;
 
         return $this->load->view($view, $this->data, $return);
     }
